77f248f382c5d02b483428098a954743bddf1eed
Suchit <siwakotisuchit22@gmail.com>
2026-08-11 12:58:28 +0545

    Fix production API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,4 +1,8 @@
-const BASE_URL = "http://localhost:3000/api";
+const BASE_URL = import.meta.env.DEV
+  ? import.meta.env.VITE_API_BASE_DEV
+  : import.meta.env.VITE_API_BASE_PROD;
+
+  export default BASE_URL;
 
 function getToken() {
   return localStorage.getItem("token");
